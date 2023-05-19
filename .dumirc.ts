676ccd69fd714a5d09ae97ea822ff7724d8192f5
冰砚炽 <Bing_Yanchi@outlook.com>
2023-05-19 20:24:23 +0800

    调整文档

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,24 +10,10 @@ export default defineConfig({
     footer: false,
     showLineNum: true,
     rtl: true,
-    hero: {
-      'zh-CN': {
-        title: '冰糖文档',
-        description: '包含了冰糖有关的所有信息的资料库',
-      },
-      'en-US': {
-        title: 'BingTang Docs',
-        description: 'A library of information containing all the information about BingTang',
-      },
-      'zh-TW': {
-        title: '冰糖文檔',
-        description: '包含了冰糖有關的所有信息的資料庫',
-      }
-    },
   },
   locales: [
     { id: 'zh-CN', name: '简体中文' },
-    { id: 'zh-TW', name: '繁体中文' },
+    { id: 'zh-TW', name: '繁體中文' },
     { id: 'en-US', name: 'English'}
   ],
   sitemap: {
