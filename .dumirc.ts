563c903196bf1d1f9f7cb86f11c46a95d810499e
冰砚炽 <Bing_Yanchi@outlook.com>
2023-05-19 18:40:29 +0800

    调整配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  resolve: {
+    docDirs: ['i18n/docs'],
+  },
   themeConfig: {
     name: 'BingTang Docs',
     github: 'https://github.com/Bing-Plugins/BingTang-Doc',
+    showLineNum: true,
+    rtl: true
   },
   locales: [
     { id: 'zh-CN', name: '简体中文' },
+    { id: 'zh-TW', name: '繁体中文' },
     { id: 'en-US', name: 'English'}
-  ]
+  ],
+  sitemap: {
+    hostname: 'sitemap.xml',
+    exclude: []
+  }
 });
