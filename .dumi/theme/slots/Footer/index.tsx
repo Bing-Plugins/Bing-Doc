5d9d8d54fee31751a524209a75e1e3d8572fd36c
冰砚炽 <Bing_Yanchi@outlook.com>
2023-05-19 11:04:18 +0800

    Update index.tsx

--- .dumi/theme/slots/Footer/index.tsx
+++ .dumi/theme/slots/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { Footer } from 'dumi-theme-antd-style';
 
 const Foot = () => {
-  return (<Footer bottom={'Copyright © 2016 BingTang Doc'} columns=undefined />);
+  return (<Footer bottom={'Copyright © 2016 BingTang Doc'} columns={undefined} />);
 }
 
 export default Foot;
