368590d6035b08d974a53f4fe4f720f243e19ee0
冰砚炽 <Bing_Yanchi@outlook.com>
2023-05-19 20:15:04 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,7 +9,21 @@ export default defineConfig({
     github: 'https://github.com/Bing-Plugins/BingTang-Doc',
     footer: false,
     showLineNum: true,
-    rtl: true
+    rtl: true,
+    hero: {
+      'zh-CN': {
+        title: '冰糖文档',
+        description: '包含了冰糖有关的所有信息的资料库',
+      },
+      'en-US': {
+        title: 'BingTang Docs',
+        description: 'A library of information containing all the information about BingTang',
+      },
+      'zh-TW': {
+        title: '冰糖文檔',
+        description: '包含了冰糖有關的所有信息的資料庫',
+      }
+    },
   },
   locales: [
     { id: 'zh-CN', name: '简体中文' },
